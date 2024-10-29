193fb4f1ba2aa0e2a5a6e38d8a1f15e0983773c3
Uczeń <Ucze@STANOWISKO8>
2024-10-29 10:20:37 +0100

    Dodaj pliki projektów.

---
+++ Counter1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Counter1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Counter1/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace Counter1;
+
+public partial class MainPage : ContentPage
+{
+    private int _counterIndex = 1;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    private void OnAddCounterClicked(object sender, EventArgs e)
+    {
+        var counterLayout = new VerticalStackLayout { Padding = 10 };
+
+        var counterName = $"Licznik {_counterIndex++}";
+
+        var nameLabel = new Label { Text = counterName, FontSize = 18, HorizontalOptions = LayoutOptions.Center };
+
+        var counterLabel = new Label { Text = "0", FontSize = 20, HorizontalOptions = LayoutOptions.Center };
+
+        var minusButton = new Button { Text = "-" };
+
+        var plusButton = new Button { Text = "+" };
+
+        int counterValue = 0;
+
+        minusButton.Clicked += (s, e) =>
+        {
+            counterValue--;
+            counterLabel.Text = counterValue.ToString();
+        };
+
+        plusButton.Clicked += (s, e) =>
+        {
+            counterValue++;
+            counterLabel.Text = counterValue.ToString();
+        };
+
+        counterLayout.Children.Add(nameLabel);
+        counterLayout.Children.Add(counterLabel);
+        counterLayout.Children.Add(new HorizontalStackLayout { Children = { minusButton, plusButton } });
+        CountersStack.Children.Add(counterLayout);
+    }
+}
